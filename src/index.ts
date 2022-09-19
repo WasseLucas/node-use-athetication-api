0e950154a4a5059a7534655e04a46f5f29c2c59b
Wasse Lucas <wasselucas.dev@gmail.com>
2022-09-19 15:04:40 -0300

    implementando index.js json-urlencoded

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 //import express
 import express, {Request, Response, NextFunction } from 'express';
+import { json } from 'stream/consumers';
 import usersRoute from './routes/users.routes';
 
 const app = express();
 
+// Configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Configuração das rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
-    res.status(200).send({foo: 'bar'});
+    res.status(200).send({foo: "Sucesso!"});
 });
-
+//Inicialização do servidor
 app.listen(3000,()=>{
     console.log('Aplicação rodando na porta 3000');
 });
