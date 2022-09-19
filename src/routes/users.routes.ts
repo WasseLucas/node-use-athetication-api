67492201eae72cb703cdeaee984c4b3ddd110c68
Wasse Lucas <wasselucas.dev@gmail.com>
2022-09-19 15:05:29 -0300

    methodo post

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -14,6 +14,11 @@ usersRoute.get('users/:uuid', (req: Request < { uuid: string } >, res: Response,
     res.status( StatusCodes.OK ).send({ uuid });
 });
 
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction)=>{
+    const newUser =  req.body;
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
 
 
 export default usersRoute;
