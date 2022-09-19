8dfab66b18d9aba55adb95d9df9468e7968f057d
Wasse Lucas <wasselucas.dev@gmail.com>
2022-09-19 15:30:40 -0300

    refatorando rota status

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 //import express
 import express, {Request, Response, NextFunction } from 'express';
 import { json } from 'stream/consumers';
+import statusRoute from './routes/status.routes';
 import usersRoute from './routes/users.routes';
 
 const app = express();
@@ -11,6 +12,8 @@ app.use(express.urlencoded({extended:true}));
 
 //Configuração das rotas
 app.use(usersRoute);
+
+app.use(statusRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
     res.status(200).send({foo: "Sucesso!"});
