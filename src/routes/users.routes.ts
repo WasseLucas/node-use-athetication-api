cbba88a606c0a3bd6f457acca83c8530ec91e37c
Wasse Lucas <wasselucas.dev@gmail.com>
2022-09-19 14:39:31 -0300

    usando StatusCode

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,17 @@
 import { NextFunction, Response, Request, Router } from "express";
+
+import {OK, StatusCodes} from 'http-status-codes'
+
 const usersRoute = Router();
 
 usersRoute.get('/users',(req: Request, res: Response, next: NextFunction)=>{
     const users = [{ userName:'Wasse' }];
-    res.status(200).json({users});
+    res.status( StatusCodes.OK ).json({ users });
 });
 
 usersRoute.get('users/:uuid', (req: Request < { uuid: string } >, res: Response, next: NextFunction)=>{
     const uuid = req.params.uuid;    
-    res.status(200).send({uuid})
+    res.status( StatusCodes.OK ).send({ uuid });
 });
 
 
