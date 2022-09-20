ca51cbff685ee02ae6fae2d6bfd71aeb2834947c
Wasse Lucas <wasselucas.dev@gmail.com>
2022-09-20 10:22:27 -0300

    docker-compose

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const status_routes_1 = __importDefault(require("./routes/status.routes"));
+const users_routes_1 = __importDefault(require("./routes/users.routes"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(users_routes_1.default);
+app.use(status_routes_1.default);
 app.get('/status', (req, res, next) => {
-    res.status(200).send({ foo: 'bar' });
+    res.status(200).send({ foo: "Sucesso!" });
 });
 app.listen(3000, () => {
     console.log('Aplicação rodando na porta 3000');
